5600bb27291d2c459e20794fc5bf1f4024bba5f2
HuertaV <112195133+HuertaV@users.noreply.github.com>
2023-10-03 13:59:48 -0700

    Added Unity Files

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData
+{
+    public static string[] descriptions =
+    {
+        "Room 0", 
+        "Room 1", 
+        "Room 2", 
+        "Room 3", 
+        "Room 4", 
+        "Room 5", 
+        "Room 6", 
+        "Room 7", 
+        "Room 8", 
+        "Room 9", 
+        "Room 10",
+        "Room 11",
+        "Room 12",
+        "Room 13",
+        "Room 14",
+        "Room 15",
+        "Room 16",
+        "Room 17",
+        "Room 18",
+        "Room 19",
+        "Room 20"
+    };
+
+/*    public static string[,] options =
+    {
+        {"Option 00", "Option 01", "Option 02", "Option 03" },
+        {"Option 10", "Option 11", "Option 12", "Option 13" },
+        {"Option 20", "Option 21", "Option 22", "Option 23" },
+        {"Option 30", "Option 31", "Option 32", "Option 33" },
+        {"Option 40", "Option 41", "Option 42", "Option 43" },
+        {"Option 50", "Option 51", "Option 52", "Option 53" },
+        {"Option 60", "Option 61", "Option 62", "Option 63" },
+        {"Option 70", "Option 71", "Option 72", "Option 73" },
+        {"Option 80", "Option 81", "Option 82", "Option 83" },
+        {"Option 90", "Option 91", "Option 92", "Option 93" },
+        {"Option 100", "Option 101", "Option 102", "Option 103" },
+        {"Option 110", "Option 111", "Option 112", "Option 113" }
+    }; */
+
+    public static string[][] options =
+    {
+        new string[]{"Do Nothing", "Investigate", "Yell For Help"}, // Start Room | 0 - > 1, 2
+        new string[]{"Give Up", "Investigate"}, // If Yell Or Do Nothing Room 1 - > 16, 2
+        new string[]{"Pick Up", "Ignore", "Investigate", "Break"}, // If Investigate | Room 2 - > 3, 4, 5, 17
+        new string[]{"Pick Up", "Ignore", "Break"}, // Investigates Stick | Room 3 - > 4, 5, 17
+        new string[]{"Continue Forward"}, // Breaks Stick | Room 4 - > 5
+        new string[]{"Start Encounter"}, // Pre-Encounter | Room 5 - > 6
+        new string[]{"Fight", "Act", "Items", "Mercy"}, // Encounter | Room 6 - > 7, 8, 9, 10
+        new string[]{"Reset", "Quit Game"}, // Fought With Stick (Ending) | Room 7
+        new string[]{"Reset", "Quit Game"}, // Fought Without Stick (Ending) | Room 8
+        new string[]{"Check", "Talk", "Joke", "Threaten"}, // Act Menu | Room 9 - > 18, 19, 15, 20
+        new string[]{"Reset", "Quit Game"}, // Items Chosen (Ending) | Room 10
+        new string[]{"Spare", "Flee"}, // Mercy Chosen | Room 11 - > 12, 13, 14
+        new string[]{"Reset", "Quit Game"}, // If Flee (Ending) | Room 12
+        new string[]{"Fight", "Act", "Items", "Mercy"}, // If Spare no talk | Room 13 - > 7, 8, 9, 10
+        new string[]{"Reset", "Quit Game"}, // If Spare talked (Ending) | Room 14
+        new string[]{"Reset", "Quit Game"}, // If joked (Ending) | Room 15
+        new string[]{"Reset", "Quit Game"}, // If Gave Up (Ending) | Room 16
+        new string[]{"Next"}, // If Picked Up Stick | Room 17 - > 5 (Sets hasStick to true)
+        new string[]{"Next"}, // If Checked | Room 18 - > 6
+        new string[]{"Next"}, // If Talked | Room 19 - > 6
+        new string[]{"Next"} // If Threats | Room 20 - > 6
+    };
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,294 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public TMP_Text display;
+    public Button[] buttons;
+    private int location;
+    public bool hasStick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GoToLocation(0); // Start Screen
+    }
+
+    private void GoToLocation(int newLocation)
+    {
+        location = newLocation;
+
+        display.text = GameData.descriptions[location];
+        int numOptions = GameData.options[location].Length;
+        //Debug.Log("Number of Options " +  numOptions);
+        for(int i = 0; i < numOptions; i++)
+        {
+            buttons[i].GetComponentInChildren<TMP_Text>().text = GameData.options[location][i];
+        }
+        for (int i = 0; i < buttons.Length; i++)
+            {
+            if(i < numOptions)
+            {
+                buttons[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                buttons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void optionSelected(int optionSelected)
+    {
+        
+        if(optionSelected == 0)
+        {
+            if(location == 0)
+            {
+                GoToLocation(1);
+            }
+            else if(location == 1)
+            {
+                GoToLocation(16);
+            }
+            else if (location == 2)
+            {
+                GoToLocation(17);
+            }
+            else if (location == 3)
+            {
+                GoToLocation(4);
+            }
+            else if (location == 4)
+            {
+                GoToLocation(5);
+            }
+            else if (location == 5)
+            {
+                GoToLocation(6);
+            }
+            else if (location == 6)
+            {
+                if (hasStick)
+                {
+                    GoToLocation(7);
+                }
+                else
+                {
+                    GoToLocation(8);
+                }
+            }
+            else if (location == 7)
+            {
+                Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+
+                Debug.Log("Retry");
+            }
+            else if (location == 8)
+            {
+                Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+
+                Debug.Log("Retry");
+            }
+            else if (location == 9)
+            {
+                GoToLocation(18);
+            }
+            else if (location == 10)
+            {
+                Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+
+                Debug.Log("Retry");
+            }
+            else if (location == 11)
+            {
+                GoToLocation(12);
+            }
+            else if (location == 12)
+            {
+                Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+
+                Debug.Log("Retry");
+            }
+            else if (location == 13)
+            {
+                if (hasStick)
+                {
+                    GoToLocation(7);
+                }
+                else
+                {
+                    GoToLocation(8);
+                }
+            }
+            else if (location == 14)
+            {
+                Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+
+                Debug.Log("Retry");
+            }
+            else if (location == 15)
+            {
+                Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+
+                Debug.Log("Retry");
+            }
+            else if (location == 16)
+            {
+                Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+
+                Debug.Log("Retry");
+            }
+            else if (location == 17)
+            {
+                hasStick = true;
+                GoToLocation(5);
+            }
+            else if (location == 18)
+            {
+                GoToLocation(6);
+            }
+            else if (location == 19)
+            {
+                GoToLocation(6);
+            }
+            else
+            {
+                GoToLocation(6);
+            }
+
+        }
+        else if (optionSelected == 1)
+        {
+            if (location == 0)
+            {
+                GoToLocation(2);
+            }
+            else if (location == 1)
+            {
+                GoToLocation(2);
+            }
+            else if (location == 2)
+            {
+                GoToLocation(4);
+            }
+            else if (location == 3)
+            {
+                GoToLocation(5);
+            }
+            else if (location == 6)
+            {
+                GoToLocation(8);
+            }
+            else if (location == 7)
+            {
+                Application.Quit();
+                Debug.Log("Quit");
+            }
+            else if (location == 8)
+            {
+                Application.Quit();
+                Debug.Log("Quit");
+            }
+            else if (location == 9)
+            {
+                GoToLocation(19);
+            }
+            else if (location == 10)
+            {
+                Application.Quit();
+                Debug.Log("Quit");
+            }
+            else if (location == 11)
+            {
+                GoToLocation(1);
+            }
+            else if (location == 12)
+            {
+                GoToLocation(13);
+            }
+            else if (location == 13)
+            {
+                GoToLocation(7);
+            }
+            else if (location == 14)
+            {
+                Application.Quit();
+                Debug.Log("Quit");
+            }
+            else if (location == 15)
+            {
+                Application.Quit();
+                Debug.Log("Quit");
+            }
+            else
+            {
+                Application.Quit();
+                Debug.Log("Quit");
+            }
+        }
+        else if (optionSelected == 2)
+        {
+            if (location == 0)
+            {
+                GoToLocation(1);
+            }
+            else if (location == 2)
+            {
+                GoToLocation(3);
+            }
+            else if (location == 3)
+            {
+                GoToLocation(4);
+            }
+            else if (location == 6)
+            {
+                GoToLocation(10);
+            }
+            else if (location == 9)
+            {
+                GoToLocation(15);
+            }
+            else
+            {
+                GoToLocation(10);
+            }
+        }
+        else
+        {
+            if (location == 2)
+            {
+                GoToLocation(2);
+            }
+            else if (location == 6)
+            {
+
+            }
+        }
+        }
+/*
+        display.text = GameData.descriptions[1];
+        buttons[0].GetComponentInChildren<TMP_Text>().text = GameData.options[1][0];
+        buttons[1].GetComponentInChildren<TMP_Text>().text = GameData.options[1][1];
+        buttons[2].GetComponentInChildren<TMP_Text>().text = GameData.options[1][2];
+        buttons[3].GetComponentInChildren<TMP_Text>().text = GameData.options[1][3];
+*/
+    }
+
+
+    public void EndGame(int index)
+    {
+        Debug.Log($"Game Over: {index}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
